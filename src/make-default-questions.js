705ef5a1ef35f771f04da11b616deb4024876cb4
Eleven <master@eleven.net.cn>
2022-07-25 11:42:51 +0000

    perf: add types, update desc

--- src/make-default-questions.js
+++ src/make-default-questions.js
@@ -6,16 +6,18 @@ module.exports = (allPackages, changedPackages) => ([
     name: 'type',
     message: 'Select the type of change that you\'re committing:',
     choices: [
-      {value: 'feat',     name: 'feat:     ✨  A new feature (note: this will indicate a release)'},
-      {value: 'fix',      name: 'fix:      🛠  A bug fix (note: this will indicate a release)'},
+      {value: 'feat',     name: 'feat:     A new feature'},
+      {value: 'fix',      name: 'fix:      A bug fix'},
       {value: 'docs',     name: 'docs:     Documentation only changes'},
       {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
       {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
       {value: 'perf',     name: 'perf:     A code change that improves performance'},
-      {value: 'test',     name: 'test:     Adding missing tests'},
-      {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
+      {value: 'test',     name: 'test:     Adding missing tests or correcting existing tests'},
+      {value: 'build',    name: 'build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'},
+      {value: 'ci',       name: 'ci:       Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)'},
+      {value: 'chore',    name: 'chore:    Other changes that do not modify src or test files'},
       {value: 'revert',   name: 'revert:   Revert to a commit'},
-      {value: 'WIP',      name: 'WIP:      Work in progress'}
+      {value: 'wip',      name: 'wip:      Work in progress'}
     ],
   },
   {
